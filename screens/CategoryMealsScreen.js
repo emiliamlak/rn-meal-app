602c7c69fa2eeb8b565ebf67ae75269549f03367
EmiliaMlak <emiliamlak@gmail.com>
2022-01-19 18:59:50 +0000

    add navigation between screens

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const CategoryMealsScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The CategoryMealsScreen!</Text>
+            <Text>The Meal Detail Screen!</Text>
+            <Button title="Go to Meal Detail!" onPress={() => {
+                props.navigation.navigate({ routeName: 'MealDetail' });
+            }} />
         </View>);
 };
 
