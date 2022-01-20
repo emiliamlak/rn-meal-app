d99f378e78565871c9572540e0f87105270927a7
EmiliaMlak <emiliamlak@gmail.com>
2022-01-20 14:26:22 +0000

    pass params from CategoriesScreen.js to CategoryMealsScreen.js

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -10,7 +10,11 @@ const CategoriesScreen = props => {
             <TouchableOpacity
                 style={styles.gridItem}
                 onPress={() => {
-                    props.navigation.navigate({ routeName: 'CategoryMeals' });
+                    props.navigation.navigate({ routeName: 'CategoryMeals',
+                params: {
+                    categoryId: itemData.item.id
+                } 
+            });
                 }}
             >
                 <View>
