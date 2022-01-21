0c5f8ded90af6b74fa94f85f6e5f50aa542706a3
EmiliaMlak <emiliamlak@gmail.com>
2022-01-21 15:46:34 +0000

    add colors and styling to categories and categories titles

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -7,8 +7,8 @@ const CategoryGridTile = props => {
             style={styles.gridItem}
             onPress={props.onSelect}
         >
-            <View>
-                <Text>
+            <View style={{ ...styles.container, ...{ backgroundColor: props.color } }}> 
+                <Text style={styles.title} numberOfLines={2}>
                     {props.title}
                 </Text>
             </View>
@@ -21,6 +21,23 @@ const styles = StyleSheet.create({
         flex: 1,
         margin: 15,
         height: 150
+    },
+    container: {
+        flex: 1,
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 10,
+        elevation: 3,
+        padding: 15,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22, 
+        textAlign: 'right'
     }
 });
 
