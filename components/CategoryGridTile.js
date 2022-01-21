87bebf1409f6da35b20479e3f3a0be0acb5398ee
EmiliaMlak <emiliamlak@gmail.com>
2022-01-21 15:55:59 +0000

    add ripple effect for categories and adjust to platform, more styling

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, View, Text } from 'react-native';
+import { TouchableOpacity, StyleSheet, View, Text, Platform, TouchableNativeFeedback } from 'react-native';
 
 const CategoryGridTile = props => {
+    let TouchableCmp = TouchableOpacity;
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        TouchableCmp = TouchableNativeFeedback;
+    }
     return (
-        <TouchableOpacity
-            style={styles.gridItem}
-            onPress={props.onSelect}
-        >
-            <View style={{ ...styles.container, ...{ backgroundColor: props.color } }}> 
-                <Text style={styles.title} numberOfLines={2}>
-                    {props.title}
-                </Text>
-            </View>
-        </TouchableOpacity>
+        <View style={styles.gridItem}>
+            <TouchableCmp
+                style={{ flex: 1 }}
+                onPress={props.onSelect}
+            >
+                <View style={{ ...styles.container, ...{ backgroundColor: props.color } }}>
+                    <Text style={styles.title} numberOfLines={2}>
+                        {props.title}
+                    </Text>
+                </View>
+            </TouchableCmp>
+        </View>
     );
 };
 
@@ -20,7 +27,9 @@ const styles = StyleSheet.create({
     gridItem: {
         flex: 1,
         margin: 15,
-        height: 150
+        height: 150,
+        borderRadius: 10,
+        overflow: 'hidden'
     },
     container: {
         flex: 1,
@@ -36,7 +45,7 @@ const styles = StyleSheet.create({
     },
     title: {
         fontFamily: 'open-sans-bold',
-        fontSize: 22, 
+        fontSize: 22,
         textAlign: 'right'
     }
 });
