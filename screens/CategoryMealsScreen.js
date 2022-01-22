355b1e9ce00ddb8f316d8bf2f7983425b229d67b
EmiliaMlak <emiliamlak@gmail.com>
2022-01-22 15:07:50 +0000

    crete logic for loading meals depnding on categories they belong to in CategoryMealsScreen.js

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
+import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 const CategoryMealsScreen = props => {
+    const renderMealItem = itemData => {
+    return (<View><Text>{itemData.item.title}</Text></View>);
+    };
+
     const catId = props.navigation.getParam('categoryId');
 
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  const displayedMeals = MEALS.filter(
+      meal => meal.categoryIds.indexOf(catId) >= 0
+  );
 
     return (
         <View style={styles.screen}>
-            <Text>The Meal Detail Screen!</Text>
-            <Text>{selectedCategory.title}</Text>
+        <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealItem}/>
             <Button title="Go to Meal Detail!" onPress={() => {
                 props.navigation.navigate({ routeName: 'MealDetail' });
             }} />
