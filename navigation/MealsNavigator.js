61dff4914c9242a827cfbc1b9482273d93b69982
EmiliaMlak <emiliamlak@gmail.com>
2022-01-28 09:33:35 +0000

    set icons and configure tabs in MealsNavigator.js

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
@@ -33,7 +35,23 @@ const MealsNavigator = createStackNavigator({
 
 
 const MealsFavTabNavigator = createBottomTabNavigator({
-    Meals: MealsNavigator,
-    Favourites: FavouritesScreen
-})
+    Meals: {
+        screen: MealsNavigator, navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />;
+            }
+        }
+    },
+    Favourites: {
+        screen: FavouritesScreen, navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />;
+            }
+        }
+    },
+}, {
+    tabBarOptions: {
+        activeTintColor: Colors.accentColor
+    }
+});
 export default createAppContainer(MealsFavTabNavigator);
