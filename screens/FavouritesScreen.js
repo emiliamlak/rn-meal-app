6d4d17ae07333749a45aa34a87caa133a5690468
EmiliaMlak <emiliamlak@gmail.com>
2022-02-12 16:33:11 +0000

    add fallback text in FavouritesScreen.js

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 
 const FavouritesScreen = props => {
-const favMeals = useSelector(state => state.meals.favouriteMeals);
+    const favMeals = useSelector(state => state.meals.favouriteMeals);
+
+
+    if (favMeals.length === 0 || !favMeals) {
+        return <View style={styles.content}>
+            <DefaultText>
+                No favourite meals found. Start adding some!
+            </DefaultText>
+        </View>
+
+    }
 
     return <MealList listData={favMeals} navigation={props.navigation} />;
 };
@@ -24,5 +36,13 @@ FavouritesScreen.navigationOptions = navData => {
         )
     }
 };
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default FavouritesScreen;
